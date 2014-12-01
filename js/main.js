9dba784f346e22f619406d5bef7b32107e0e1a2c
Nick Roberts <nickroberts404@gmail.com>
2014-11-30 19:15:08 -0600

    Created the apps backbone, made example table.

---
+++ js/main.js
@@ -0,0 +1,25 @@
+(function(){
+
+    
+    for(ratioList in ratios){
+        dynamicDimensions(ratioList, ratios[ratioList]);
+    }
+
+    function dynamicDimensions(selector, properties){
+        console.log($('#candy-wrapper').css('width'));
+        var windowWidth = window.innerWidth;
+        var theSelector = $(selector);
+        for(property in properties){
+            var style = property;
+            var ratio = properties[property];
+            newDimension = windowWidth*ratio
+            theSelector.css(style, newDimension); 
+        } 
+    }
+
+    $(window).on('resize', function(){
+        for(ratioList in ratios){
+            dynamicDimensions(ratioList, ratios[ratioList]); 
+        } 
+    });
+})();
