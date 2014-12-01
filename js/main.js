f39dc36596636335d1a7cea0b451a19b936038c6
Nick Roberts <nickroberts404@gmail.com>
2014-11-30 22:59:44 -0600

    Added chroma.js and coloring functions.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 (function(){
-
     
     for(ratioList in ratios){
         dynamicDimensions(ratioList, ratios[ratioList]);
@@ -12,7 +11,6 @@
     });
 
     function dynamicDimensions(selector, properties){
-        console.log($('#candy-wrapper').css('width'));
         var windowWidth = window.innerWidth;
         var theSelector = $(selector);
         for(property in properties){
@@ -21,6 +19,54 @@
             newDimension = windowWidth*ratio
             theSelector.css(style, newDimension); 
         } 
+    }
+
+    var findColor = function(elementID){
+        var type = theElements[elementID].type;
+        if(type=='alkali-metal') return alkaliColor;
+        if(type=='alkaline-earth') return alkalineEarthColor;
+        if(type=='transition-metal') return transitionColor;
+        if(type=='post-transition-metal') return postTransitionColor;
+        if(type=='metalloid') return metalloidColor;
+        if(type=='other-nonmetal') return otherColor;
+        if(type=='halogen') return halogenColor;
+        if(type=='noble-gas') return nobleColor;
+        if(type=='lanthanoid') return lanthanoidColor;
+        if(type=='actinoid') return actinoidColor;
+    }
+
+    var colorElements = function(elementID){
+        if(typeof elementID == 'string') elementID = [elementID];
+        for(var i=0; i<elementID.length; i++){
+            color = findColor(elementID[i]);
+            $('#'+elementID[i]).css('background-color', color.hex());
+        }
+    }
+
+    var grayscaleElements = function(elementID){
+        if(typeof elementID == 'string') elementID = [elementID];
+        for(var i=0; i<elementID.length; i++){
+            color = findColor(elementID[i]);
+            $('#'+elementID[i]).css('background-color', grayscale(color).hex());
+        }
+    }
+
+    function grayscale(color){
+        color = color.rgba();
+        console.log(color);
+        // Normal Averaging Algorithm
+        // gray = (color[0]+color[1]+color[2])/3;
+        // Luma Algorithm
+        gray = color[0]*0.3+color[1]*0.59+color[2]*0.11;
+        return chroma(gray, gray, gray, color[3]);
+    }
+
+    forEachElement(colorElements);
+
+    function forEachElement(aFunction){
+        for(element in theElements){
+            aFunction(element);
+        }
     }
     
     
