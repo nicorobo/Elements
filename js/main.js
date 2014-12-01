a42669985412e11615dbb729262b5f22a0e0559d
Nick Roberts <nickroberts404@gmail.com>
2014-11-30 23:42:45 -0600

    Placed colors in an associative array. Reorganized javascript.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,46 @@
 (function(){
+
+  ////////////////////////////////////////////
+ ///////  Initialization Functions  /////////
+////////////////////////////////////////////
     
     for(ratioList in ratios){
         dynamicDimensions(ratioList, ratios[ratioList]);
+    }
+
+    forEachElement(colorElements);
+
+
+  ////////////////////////////////////////////
+ ///////////  Styling Functions  ////////////
+////////////////////////////////////////////
+
+
+
+    function colorElements(elementID){
+        if(typeof elementID == 'string') elementID = [elementID];
+        for(var i=0; i<elementID.length; i++){
+            color = typeColors[theElements[elementID].type];
+            $('#'+elementID[i]).css('background-color', color.hex());
+        }
+    }
+
+    var grayElements = function(elementID){
+        if(typeof elementID == 'string') elementID = [elementID];
+        for(var i=0; i<elementID.length; i++){
+            color = typeColors[theElements[elementID].type];
+            $('#'+elementID[i]).css('background-color', grayscale(color).hex());
+        }
+    }
+
+    function grayscale(color, amount){
+        if(!amount) var amount = 1;
+        var color = color.rgba();
+        // Normal Averaging Algorithm
+        // gray = (color[0]+color[1]+color[2])/3;
+        // Luma Algorithm
+        gray = color[0]*0.3+color[1]*0.59+color[2]*0.11;
+        return chroma((color[0]*(1-amount))+gray*amount, (color[1]*(1-amount))+gray*amount, (color[2]*(1-amount))+gray*amount, color[3]);
     }
 
     $(window).on('resize', function(){
@@ -21,47 +60,13 @@
         } 
     }
 
-    var findColor = function(elementID){
-        var type = theElements[elementID].type;
-        if(type=='alkali-metal') return alkaliColor;
-        if(type=='alkaline-earth') return alkalineEarthColor;
-        if(type=='transition-metal') return transitionColor;
-        if(type=='post-transition-metal') return postTransitionColor;
-        if(type=='metalloid') return metalloidColor;
-        if(type=='other-nonmetal') return otherColor;
-        if(type=='halogen') return halogenColor;
-        if(type=='noble-gas') return nobleColor;
-        if(type=='lanthanoid') return lanthanoidColor;
-        if(type=='actinoid') return actinoidColor;
-    }
 
-    var colorElements = function(elementID){
-        if(typeof elementID == 'string') elementID = [elementID];
-        for(var i=0; i<elementID.length; i++){
-            color = findColor(elementID[i]);
-            $('#'+elementID[i]).css('background-color', color.hex());
-        }
-    }
 
-    var grayscaleElements = function(elementID){
-        if(typeof elementID == 'string') elementID = [elementID];
-        for(var i=0; i<elementID.length; i++){
-            color = findColor(elementID[i]);
-            $('#'+elementID[i]).css('background-color', grayscale(color).hex());
-        }
-    }
+  ////////////////////////////////////////////
+ ///////////  Utility Functions  ////////////
+////////////////////////////////////////////
 
-    function grayscale(color){
-        color = color.rgba();
-        console.log(color);
-        // Normal Averaging Algorithm
-        // gray = (color[0]+color[1]+color[2])/3;
-        // Luma Algorithm
-        gray = color[0]*0.3+color[1]*0.59+color[2]*0.11;
-        return chroma(gray, gray, gray, color[3]);
-    }
 
-    forEachElement(colorElements);
 
     function forEachElement(aFunction){
         for(element in theElements){
